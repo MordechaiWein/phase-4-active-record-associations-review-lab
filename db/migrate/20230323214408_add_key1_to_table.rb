abe1f3d22293b77af2ff8510d44128834e16d21b
Mordechai <mordwein77@gmail.com>
2023-04-23 18:07:48 -0400

    commit tree 6a96a54bf83c5cee9d168697423a2e06b6a5976f

---
+++ db/migrate/20230323214408_add_key1_to_table.rb
@@ -0,0 +1,5 @@
+class AddKey1ToTable < ActiveRecord::Migration[6.1]
+  def change
+    add_column :rides, :taxi_id, :integer
+  end
+end
---
+++ db/migrate/20230323214526_add_key1_to_table2.rb
@@ -0,0 +1,5 @@
+class AddKey1ToTable2 < ActiveRecord::Migration[6.1]
+  def change
+    add_column :rides, :passenger_id, :integer
+  end
+end
